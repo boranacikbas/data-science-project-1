768ddb44a115d7452729b87f2701fedcb05479e3
Boran <boranacikbas@gmail.com>
2026-07-08 19:59:56 +0300

    Feat: 10 adet analitik SQL sorgusu tamamlandı, dsdb şeması entegre edildi

---
+++ Proje 1 Çözümleri.sql
@@ -0,0 +1,21 @@
+SELECT * FROM customers
+
+SELECT * FROM orders ORDER BY total_amount DESC LIMIT 5
+SELECT * FROM products
+
+SELECT product_name, price FROM products ORDER BY price ASC LIMIT 3
+
+SELECT * FROM customers ORDER BY signup_date ASC LIMIT 
+
+SELECT product_name, stock_quantity FROM products ORDER BY stock_quantity DESC LIMIT 1
+SELECT * FROM orders ORDER BY order_date DESC LIMIT 1
+
+SELECT product_name FROM products ORDER BY product_name ASC 
+
+SELECT customer_id, email FROM customers ORDER BY customer_id LIMIT 5
+
+SELECT order_id, total_amount FROM orders ORDER BY total_amount LIMIT 3
+
+SELECT * FROM customers 
+WHERE country='Turkey'
+ORDER BY customer_name
